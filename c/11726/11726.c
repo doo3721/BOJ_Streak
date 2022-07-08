08477a149c16ef4e1b0e57a71605779723005558
김두희 <kimduheui@gimduhuiui-MacBookAir.local>
2022-07-08 17:37:10 +0900

    11726

---
+++ c/11726/11726.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+static int	dp[1001] = {0, };
+
+int main(void)
+{
+	int	n, idx;
+
+	scanf("%d", &n);
+
+	dp[1] = 1;
+	dp[2] = 2;
+	idx = 3;
+	while (idx <= n)
+	{
+		dp[idx] = (dp[idx - 1] + dp[idx - 2]) % 10007;
+		idx++;
+	}
+	printf("%d", dp[n]);
+	return (0);
+}
