d582613a8ca42aa3521197f31eefdad75ba5e006
김두희 <kimduheui@gimduhuiui-MacBookAir.local>
2022-07-21 22:08:50 +0900

    11054

--- c/11054/11054.c
+++ c/11054/11054.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-static int	arr[1001];
-static int	dp[1001];
+static int	arr[1000];
+static int	s_dp[1000];
+static int	r_dp[1000];
 
 static int	ft_max(int a, int b)
 {
@@ -18,19 +19,39 @@ int	main(void)
 	while (idx < n)
 		scanf("%d", &arr[idx++]);
 	
-	max = 0;
 	idx = 0;
 	while (idx < n)
 	{
-		dp[idx] = 1;
+		s_dp[idx] = 1;
 		s_idx = 0;
 		while (s_idx < idx)
 		{
 			if (arr[s_idx] < arr[idx])
-				dp[idx] = ft_max(dp[idx], dp[s_idx] + 1);
+				s_dp[idx] = ft_max(s_dp[idx], s_dp[s_idx] + 1);
 			s_idx++;
 		}
-		max = ft_max(max, dp[idx]);
+		idx++;
+	}
+
+	idx = n - 1;
+	while (idx >= 0)
+	{
+		r_dp[idx] = 1;
+		s_idx = n - 1;
+		while (s_idx > idx)
+		{
+			if (arr[s_idx] < arr[idx])
+				r_dp[idx] = ft_max(r_dp[idx], r_dp[s_idx] + 1);
+			s_idx--;
+		}
+		idx--;
+	}
+
+	max = 0;
+	idx = 0;
+	while (idx < n)
+	{
+		max = ft_max(max, s_dp[idx] + r_dp[idx] - 1);
 		idx++;
 	}
 	printf("%d", max);
