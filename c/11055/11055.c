1c7847a074afc5f19aa8688d4307e789176d0372
김두희 <kimduheui@gimduhuiui-MacBookAir.local>
2022-07-23 12:54:57 +0900

    11055

---
+++ c/11055/11055.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+static int	arr[1000];
+static int	dp[1000];
+
+static int	ft_max(int a, int b)
+{
+	return (a > b ? a : b);
+}
+
+int	main(void)
+{
+	int n, idx, s_idx, max;
+
+	scanf("%d", &n);
+
+	idx = 0;
+	while (idx < n)
+		scanf("%d", &arr[idx++]);
+
+	idx = 0;
+	max = 0;
+	while (idx < n)
+	{
+		dp[idx] = arr[idx];
+		s_idx = 0;
+		while (s_idx < idx)
+		{
+			if (arr[s_idx] < arr[idx])
+				dp[idx] = ft_max(dp[idx], dp[s_idx] + arr[idx]);
+			s_idx++;
+		}
+		max = ft_max(max, dp[idx]);
+		idx++;
+	}
+	printf("%d", max);
+	return (0);
+}
