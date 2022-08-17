7e872d94adfc18fc3419619f031acadd82e63644
김두희 <kimduheui@gimduhuiui-MacBookAir.local>
2022-08-17 11:03:25 +0900

    1264

--- c/1264/1264.c
+++ c/1264/1264.c
@@ -1,22 +1,7 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <string.h>
 
-static char	*v = "aeiouAEIOU";
-
-static int	ft_strchr(char t)
-{
-	int i;
-
-	i = 0;
-	while (v[i])
-	{
-		if (v[i] == t)
-			return (i);
-		i++;
-	}
-	return (-1);
-}
-
+/*
 int main(void)
 {
 	char	s[260];
@@ -31,7 +16,7 @@ int main(void)
 		i = 0;
 		while (s[i] != '\n')
 		{
-			if (ft_strchr(s[i]) != -1)
+			if (strchr("aeiouAEIOU", s[i]))
 				c++;
 			i++;
 		}
@@ -40,4 +25,33 @@ int main(void)
 	return (0);
 
 	// 왜 틀리는거지?
+}
+*/
+
+int main(void)
+{
+	char	s[260];
+	int		i, c;
+	
+	c = 0;
+	while (scanf("%s", s) > 0)
+	{
+		if (s[0] == '#')
+			break ;
+		
+		i = 0;
+		while (s[i])
+		{
+			if (strchr(".?!", s[i]))
+			{
+				printf("%d\n", c);
+				c = 0;
+				break ;
+			}
+			if (strchr("aeiouAEIOU", s[i]))
+				c++;
+			i++;
+		}
+	}
+	return (0);
 }
